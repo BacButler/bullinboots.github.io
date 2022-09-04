353b35185319480af5bb0194db998a6a817b628e
Napat Jinsuksomboon <90429771+BullInBoots@users.noreply.github.com>
2022-09-04 09:48:45 +0630

    Update piano.js

--- js/piano.js
+++ js/piano.js
@@ -8,11 +8,13 @@ const keys = document.querySelectorAll('.key')
 const whiteKeys = document.querySelectorAll('.key.white')
 const blackKeys = document.querySelectorAll('.key.black')
 
-// ใช้for loopในการใส่functionลงไปในแต่ละ
+// ใช้for loopในการใส่functionที่เมื่อถูกclickจะรันคำสั่งลงไปใน element ที่มีkeywordเป็นkeyในHTML
+// เมื่อถูก"click" จะรันคำสั่งplaynote
 keys.forEach(key => {
     key.addEventListener('click' , () => playNote(key))
 })
 
+// รับค่าจากคีย์บอร์ดเมื่อมีปุ่มใดปุ่มนึงถูกกด
 document.addEventListener('keydown', e => {
     if (e.repeat) return
     const key = e.key
@@ -24,12 +26,14 @@ document.addEventListener('keydown', e => {
 
 })
 
+// functionนี้จะใช้ในบรรทัดที่12
+// เป็นคำสั่งในการเล่นตัวโน้ต
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note)
     noteAudio.currentTime = 0
     noteAudio.play()
     key.classList.add('active')
-    setTimeout(() => {  key.classList.remove('active'); }, 500)
+    setTimeout(() => {  key.classList.remove('active'); }, 500) // ใส่ดีเลย์
     console.log(setTimeout(() => {  key.classList.remove('active'); }, 500))
 
 }
